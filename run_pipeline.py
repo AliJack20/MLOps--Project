364181a6a56d294618a04b799af0c49e22c0d21d
Ali Siddiqi <alisiddiqui275@gmail.com>
2025-08-03 17:38:09 +0500

    data prep handled

--- run_pipeline.py
+++ run_pipeline.py
@@ -3,5 +3,5 @@ from pipelines.training_pipeline import train_pipeline
 if __name__ == "__main__":
     train_pipeline(data_path="C:/Users/Ali/DataScience_Projects/MLOps/storage/olist_customers_dataset.csv")
 
-# 1:07:36
+# 1:33:44
 # https://www.youtube.com/watch?v=-dJPoLm_gtE
