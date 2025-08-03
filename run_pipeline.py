d277ea047db8db1d695fb6e2040a4458e811092a
Ali Siddiqi <alisiddiqui275@gmail.com>
2025-08-03 17:11:48 +0500

    blueprint up

---
+++ run_pipeline.py
@@ -0,0 +1,7 @@
+from pipelines.training_pipeline import train_pipeline
+
+if __name__ == "__main__":
+    train_pipeline(data_path="C:/Users/Ali/DataScience_Projects/MLOps/storage/olist_customers_dataset.csv")
+
+# 1:07:36
+# https://www.youtube.com/watch?v=-dJPoLm_gtE
