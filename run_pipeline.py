5f99fa1c876279caa7ffe39722da8d9b4c96e7d9
Ali Siddiqi <alisiddiqui275@gmail.com>
2025-10-25 13:15:42 +0500

    Making changes

--- run_pipeline.py
+++
@@ -1,9 +0,0 @@
-from pipelines.training_pipeline import train_pipeline
-from zenml.client import Client
-
-if __name__ == "__main__":
-    print(Client().active_stack.experiment_tracker.get_tracking_uri())
-    train_pipeline(data_path="C:/Users/Ali/DataScience_Projects/MLOps/olist_customers_dataset.csv")
-
-# 2:25:58
-# https://www.youtube.com/watch?v=-dJPoLm_gtE
--- steps/config.py
+++
@@ -1,6 +0,0 @@
-#from zenml.steps import BaseParameter
-from pydantic import BaseModel
-
-class ModelNameConfig(BaseModel):
-
-    model_name: str = "LinearRegression"
